85169edf6b9ca053fcee0025bb272fd913f05dbb
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-09-07 18:41:13 +0200

    Add custom assert method

--- test/disco_test.rb
+++ test/disco_test.rb
@@ -4,6 +4,13 @@ class DiscoTest < ActiveSupport::TestCase
   include Disco::MarkdownHelper
 
   test "should parse simple markdown" do
-    assert_equal "<p><em>hello</em></p>\n", markdown("*hello*")
+    assert_markdown "<p><em>hello</em></p>", "*hello*"
+    assert_markdown "<p><strong>hello</strong></p>", "**hello**"
+  end
+
+  private
+
+  def assert_markdown(expected_html, mkd, options = {})
+    assert_equal expected_html + "\n", markdown(mkd, options)
   end
 end
