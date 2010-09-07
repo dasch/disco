86305a2cc2638424003402a6824e52e01b2a7907
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-09-07 19:58:01 +0200

    Add a gemspec

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 gem 'rdiscount', '~> 1.6'
 gem 'activesupport', '~> 3.0'
+gem 'actionpack', '~> 3.0'
--- lib/disco.rb
+++ lib/disco.rb
@@ -4,5 +4,6 @@ require 'bundler/setup'
 Bundler.require(:default)
 
 require 'disco/markdown_helper'
+require 'action_view'
 
 ActionView::Base.send(:include, Disco::MarkdownHelper)
