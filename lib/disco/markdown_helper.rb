364b51a4f745992dc62a25d1aca23d49b0ae9977
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-09-07 20:52:27 +0200

    Add documentation

--- lib/disco/markdown_helper.rb
+++ lib/disco/markdown_helper.rb
@@ -3,6 +3,13 @@ require 'rdiscount'
 
 module Disco
   module MarkdownHelper
+    # Process the Markdown string +mkd+ using the RDiscount parser.
+    #
+    # If <tt>:allow_html => true</tt> is supplied, HTML code will not be
+    # escaped.
+    #
+    # The returned string is <tt>html_safe</tt> and can be used directly
+    # in your views.
     def markdown(mkd, options = {})
       d = RDiscount.new(mkd)
 
